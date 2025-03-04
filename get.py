de0ea3ded7c42b1559fa8f8e52eccd9379de70df
Thasneem Sulthana <thasneem.yzone@gmail.com>
2025-03-04 20:11:23 +0530

    Update

---
+++ get.py
@@ -0,0 +1,16 @@
+import json
+import boto3
+
+def lambda_handler(event, context):
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-2')
+
+    table = dynamodb.Table('studentData')
+
+    response = table.scan()
+    data = response['Items']
+
+    while 'LastEvaluatedKey' in response:
+        response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
+        data.extend(response['Items'])
+
+    return data
